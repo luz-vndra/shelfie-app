1f90dd86635f26e3dfd8e1fb0ff0bf39cf19bbba
fc311 <luz.vndra@proton.me>
2025-12-22 21:35:41 +0530

    Update index.tsx
    added styles to <View> and <Text> RNElements with the Stylesheet object at the bottom of the home page index file

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={ {marginTop: 10, marginBottom: 30} }>Reading List App</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+    }
+})
