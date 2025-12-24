78679fdbb2780395ab1cb0eee653b8ec4fb76cb8
fc311 <luz.vndra@proton.me>
2025-12-24 14:42:50 +0530

    added a stack handler in a new layout file

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#DDD",
+        },
+        headerTintColor: "#333",
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen
+        name="contact"
+        options={{ title: "Contact", headerShown: false }}
+      />
+    </Stack>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
