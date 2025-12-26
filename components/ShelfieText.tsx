c80663eb129a05b50941e235008b12dd78b8cf31
Lüz Itachi Vndra <luz.vndra@proton.me>
2025-12-26 17:35:55 +0530

    Create ShelfieText.tsx

---
+++ components/ShelfieText.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Text, TextStyle, useColorScheme, TextProps } from "react-native";
+import { Colors } from "../constants/Colors";
+
+// type ShelfieTextProps = React.ComponentProps<typeof Text>;
+interface ShelfieTextProps extends TextProps {
+  style?: TextStyle;
+  title?: boolean;
+}
+
+const ShelfieText: React.FC<ShelfieTextProps> = ({
+  style,
+  title = false,
+  ...props
+}) => {
+  const colorScheme = useColorScheme() ?? "light";
+  const theme = Colors[colorScheme];
+
+  const textColor = title ? theme.title : theme.text;
+
+  return <Text style={[{ color: textColor }, style]} {...props} />;
+};
+
+export default ShelfieText;
