dbd3061f222fa535f09e1de6b34afa89f612cbe1
Lüz Itachi Vndra <luz.vndra@proton.me>
2025-12-30 16:33:45 +0530

    Update ShelfieView.tsx
    established use of safeAreaInsets hook in the ShelfieView themed (primitive) component

--- components/ShelfieView.tsx
+++ components/ShelfieView.tsx
@@ -8,6 +8,7 @@ import { View, SafeAreaView, useColorScheme } from "react-native";
 // This component is deprecated and will be removed in a future release.
 
 import { Colors } from "../constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type ShelfieViewProps = React.ComponentProps<typeof View>;
 
@@ -15,9 +16,25 @@ const ShelfieView: React.FC<ShelfieViewProps> = ({ style, ...props }) => {
   const colorScheme = useColorScheme() ?? "light";
   const theme = Colors[colorScheme];
 
+  // ideally,
+  const insets = useSafeAreaInsets();
+  // - maybe can be used right at the base layout?
+  // - even before using the custom Shelfie View?
+
   return (
-    // <View style={[{backgroundColor: theme.background}, style]} {...props}/>
-    <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    <View
+      style={[
+        {
+          backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+        style,
+      ]}
+      {...props}
+    />
   );
 };
 
